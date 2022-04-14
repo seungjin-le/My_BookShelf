19040e70740e0ed544723b8e3cdbcba39b16b39d
LeeSeungJin <dltmdwls154@gmail.com>
2022-04-14 23:59:55 +0900

    Todo add

--- src/modules/index.tsx
+++ src/modules/index.tsx
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
+import todos from './Todos';
 
+// Counter, TodoList
 const rootReducer = combineReducers({
   counter,
+  todos,
 });
 
 export default rootReducer;
