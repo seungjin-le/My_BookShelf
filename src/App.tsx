064b0a0a8544fc37e1d7749b71f58d6561a400cf
LeeSeungJin <dltmdwls154@gmail.com>
2022-04-15 00:03:08 +0900

    TodoList 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Home from './page/home';
 import CounterContainer from './containers/CounterContainer';
+import TodoApp from './containers/TodoApp';
 const App = () => {
   return (
     <div>
       <Home title={'React + TypeScript'} text={'React-Redux test'} />
       <CounterContainer />
+      <hr />
+      <TodoApp />
     </div>
   );
 };
